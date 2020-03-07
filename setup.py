05a2203e4a84de6b36f27b9c677f96130d5f4a43
sehan10 <sehan_ahmed@live.com>
2020-03-07 18:04:41 +0500

    resolving issue

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'helloworld',         # How you named your package folder (MyLib)
-  version='0.0.1',
+  name = 'sehan_helloworld',         # How you named your package folder (MyLib)
+  version='0.0.2',
   description='nothing special',
   package_dir={'':'src'},
   py_modules=['helloworld']
